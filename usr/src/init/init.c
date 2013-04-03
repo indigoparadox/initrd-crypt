32b73d6e302c7932b31f6b3762efe648d4c88b09
indigoparadox <indigo.repo@interfinitdynamics.info>
2013-04-03 07:19:36 -0400

    Fleshing out the config for the new binary init.

--- usr/src/init/init.c
+++ usr/src/init/init.c
@@ -1,10 +1,19 @@
 
 #include <stdio.h>
 
+#ifndef HOSTNAME_H
+#error "No hostname specified!"
+#endif /* HOSTNAME */
+
 #include "crysco.h"
 
 int main( void ) {
 
+   /* Mount system directories. */
+
+   /* TODO: Start the splash screen (deprecated). */
+
+   /* Load any directed kernel modules. */
 
    return 0;
 }
