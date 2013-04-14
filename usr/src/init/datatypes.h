44b330901e720ec4e416339498e9daca194f205f
indigoparadox <indigo.repo@interfinitdynamics.info>
2013-04-14 13:19:07 -0400

    Rough structure and system mounter for new init in place.

--- usr/src/init/datatypes.h
+++ usr/src/init/datatypes.h
@@ -11,5 +11,13 @@ typedef struct {
    struct bstrList* devs;
 } MD_ARRAY;
 
+#ifndef BOOL
+typedef int BOOL;
+#ifndef TRUE
+#define TRUE 1
+#define FALSE 0
+#endif /* TRUE */
+#endif /* BOOL */
+
 #endif /* DATATYPES_H */
 
--- usr/src/init/init.c
+++ usr/src/init/init.c
@@ -1,6 +1,8 @@
 
 #include <stdio.h>
 #include <dirent.h>
+#include <unistd.h>
+#include <sys/mount.h>
 
 #include "bstrlib/bstrlib.h"
 
@@ -10,12 +12,41 @@
 
 #include "crysco.h"
 
+/* Purpose: Mount /usr before boot for compatbility with newer systems.       */
 int probe_usr( void ) {
    /* TODO: Open fstab and see if a line exists for /usr. Mount if applicable.
     */
    return -1;
 }
 
+/* Purpose: Prepare system mounts for a minimally functioning system.         */
+int mount_sys( BOOL b_umount_in ) {
+   if( b_umount_in ) {
+      if(
+         umount2( "/sys", MNT_FORCE ) ||
+         umount2( "/proc", MNT_FORCE ) ||
+         umount2( "/dev/pts", MNT_FORCE ) ||
+         umount2( "/dev", MNT_FORCE )
+      ) {
+         perror( "Unable to unmount one or more special filesystems" );
+         return -1;
+      }
+   } else {
+      if(
+         mount( NULL, "/dev", "devtmpfs", 0, "" ) ||
+         mount( "devpts", "/dev/pts", "devpts", 0, "" ) ||
+         mount( NULL, "/proc", "proc", 0, "" ) ||
+         mount( NULL, "/sys", "sysfs", 0, "" )
+      ) {
+         perror( "Unable to mount one or more special filesystems" );
+         return -1;
+      }
+   }
+
+   return 0;
+}
+
+/* Purpose: Setup /dev/md devices if any exist.                               */
 int mount_mds( void ) {
    int i_md_iter,
       i_dev_iter,
@@ -50,18 +81,36 @@ int mount_mds( void ) {
    return 0;
 }
 
+/* Purpose: Wait until the main devices are decrypted and start the system.   */
 void action_crypt( void ) {
 
+   /* FIXME: Try to start network listener. */
+
+   /* Get the user password. */
+   prompt_decrypt();
+
+   /* Try to premount /usr, because of the new udev. */
+   probe_usr();
 }
 
 int main( int argc, char* argv[] ) {
    int i;
+   BOOL b_decrypt_success = FALSE;
 
-   mount_mds();
+   if( 1 == getpid() ) {
+      /* We're being called as init, so set the system up. */
+      mount_sys( FALSE );
+      mount_mds();
 
+      /* TODO: Load any directed kernel modules. */
+
+      /* TODO: Start the splash screen (deprecated). */
+   }
+
+   /* See if we're being called as a prompt only. */
    for( i = 1 ; i < argc ; i++ ) {
       if( !strncmp( "-p", argv[i], 2 ) ) {
-         /* Just prompt to decrypt and exit (signalling main process to clean *
+         /* Just prompt to decrypt and exit (signaling main process to clean  *
           * up if decrypt is successful!)                                     */
          if( !prompt_decrypt() ) {
          }
@@ -69,15 +118,17 @@ int main( int argc, char* argv[] ) {
       }
    }
 
+   /* Act based on the system imperative. */
    action_crypt();
 
-   /* Mount system directories. */
-
-   /* TODO: Start the splash screen (deprecated). */
-
-   /* Load any directed kernel modules. */
-
 main_cleanup:
+   if( 1 == getpid() ) {
+      /* Prepare the system to load the "real" init. */
+      if( b_decrypt_success ) {
+         probe_usr();
+      }
+      mount_sys( FALSE );
+   }
 
    return 0;
 }
