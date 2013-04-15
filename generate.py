bc35523d16c34c5e66cb08ef921cee552f809ece
indigoparadox <indigo.repo@interfinitdynamics.info>
2013-04-15 11:27:08 -0400

    Generation script can build a rough initrd image (shouldn't totally work yet), but only from the project directory. Need to smooth it for system integration.

--- generate.py
+++ generate.py
@@ -32,14 +32,81 @@ def fix_perms( path, fmod, recursive=False, ftype=None, fname=None ):
 
    return True
 
-def build_image():
+def copy_binary( binary_path, destination, static=None, root='/' ):
+
+   ''' Attempt to copy a binary from a binlist. Return False on failure. '''
+
+   my_logger = logging.getLogger( 'initrd.copy' )
+
+   # Make sure the source file is where it should be.
+   # TODO: Why do we look under /usr? Shouldn't the binlist specify that?
+   binary_test = os.path.join( root, binary_path );
+   if 's' == static and os.path.isfile( '{}.static'.format( binary_test ) ):
+      binary_test = '{}.static'.format( binary_test )
+   else:
+      if not os.path.isfile( binary_test ):
+         binary_test = os.path.join( root, 'usr', binary_path )
+         if not os.path.isfile( binary_test ):
+            my_logger.error(
+               'Unable to locate {} for copy.'.format( binary_path )
+            )
+            return False
+
+   my_logger.info( 'Copying {}...'.format( binary_test ) )
+
+   # Make sure binary is static/dynamic as specified.
+   binary_static = None
+   proc_test = subprocess.Popen(
+      ['ldd', binary_test], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+   )
+   try:
+      binary_static = proc_test.communicate()[0].index( 'not a dynamic' )
+   except:
+      pass
+
+   if 'ld-linux.so.2' == os.path.basename( binary_test ):
+      # This is a special case.
+      binary_static = None
+
+   if 's' == static and 1 != binary_static:
+      my_logger.error(
+         '{} should be static and is dynamic.'.format( binary_test )
+      )
+      return False
+   elif 'l' == static and 1 == binary_static:
+      my_logger.error(
+         '{} should be dynamic and is static.'.format( binary_test )
+      )
+      return False
+
+   # Perform the actual copy.
+   shutil.copy(
+      binary_test,
+      os.path.join( destination, binary_path )
+   )
+
+   return True
+
+def ld_version( root='/' ):
+
+   ''' Return the current ld library version. '''
+
+   ld_pattern = re.compile( r'ld-([0-9\.]*)\.so' )
+   for entry in os.listdir( os.path.join( root, 'lib' ) ):
+      ld_match = ld_pattern.match( entry )
+      if None == ld_match:
+         continue
+      else:
+         return ld_match.groups()[0]
+
+def build_image( host_path, hostname, bincopy_root='/' ):
    try:
       my_logger = logging.getLogger( 'initrd.build' )
 
       temp_path = tempfile.mkdtemp()
       current_path = os.getcwd()
 
-      # FIXME: Copy the source to a working directory to make final tweaks.
+      # Copy the source to a working directory to make final tweaks.
       shutil.copytree(
          # FIXME: Specify image template path.
          'image',
@@ -52,6 +119,22 @@ def build_image():
       os.chdir( os.path.join( temp_path, 'initrd' ) )
 
       # TODO: Perform the integration from the host-specific binary list.
+      ld_ver = ld_version( root=bincopy_root )
+      with open(
+         os.path.join( host_path, '{}.binlist'.format( hostname ) ), 'r'
+      ) as binlist_file:
+         binline_pattern = re.compile( r'(\S*)\s*([sl]?)' )
+         for line in binlist_file:
+            binline_match = binline_pattern.match( line )
+            line_path = binline_match.groups()[0].format( ld_ver )
+            if not copy_binary(
+               line_path,
+               os.path.join( temp_path, 'initrd' ),
+               static=binline_match.groups()[1],
+               root=bincopy_root
+            ):
+               my_logger.error( 'Error copying file. Aborting.' )
+               return
 
       # Fix internal permissions and ownership.
       my_logger.info( 'Fixing permissions...' )
@@ -71,19 +154,23 @@ def build_image():
       # TODO: Randomize or allow specification of a file modification date.
 
       # Perform the actual build of the image.
+      my_logger.info( 'Compressing image...' )
       proc_find = \
          subprocess.Popen( ['find', '.', '-print0'], stdout=subprocess.PIPE )
       proc_cpio = subprocess.Popen(
          ['cpio', '--null', '-ov', '--format=newc'],
          stdin=proc_find.stdout,
-         stdout=subprocess.PIPE
+         stdout=subprocess.PIPE,
+         stderr=subprocess.PIPE
       )
 
       # Open up the target archive file and dump the compressed data into it.
-      image_path = os.path.join( current_path, 'initrd.gz' )
+      image_path = os.path.join( current_path, 'build', 'initrd.gz' )
       with gzip.open( image_path, 'wb', compresslevel=9 ) as output:
          for chunk in iter( lambda: proc_cpio.stdout.read( 8192 ), '' ):
             output.write( chunk )
+
+      my_logger.info( 'Image written to {}.'.format( image_path ) )
 
       os.chdir( current_path )
 
@@ -95,7 +182,7 @@ def build_image():
          if 2 != e.errno:
             raise
 
-def compile_init( host_path, hostname ):
+def compile_init( host_path, hostname, release=False ):
    try:
       temp_path = tempfile.mkdtemp()
       current_path = os.getcwd()
@@ -130,7 +217,10 @@ def compile_init( host_path, hostname ):
 
       # Perform the compile and copy the result back here.
       os.chdir( os.path.join( temp_path, 'init' ) )
-      subprocess.call( ['make'] )
+      command = ['make']
+      if release:
+         command += ['release']
+      subprocess.check_call( command )
       os.chdir( current_path )
       try:
          os.mkdir( 'build' )
@@ -176,11 +266,11 @@ def main():
    random.seed()
 
    my_logger.info( 'Compiling init...' )
-   compile_init( args.host_path, args.hostname )
+   compile_init( args.host_path, args.hostname, release=not args.compile_only )
 
    if not args.compile_only:
       my_logger.info( 'Building image...' )
-      build_image()
+      build_image( args.host_path, args.hostname )
 
 if __name__ == '__main__':
    main()
